e98ae12e9ab9132834ab0b5f6ae1680d3afe76c4
610983691 <tongjie.job@foxmail.com>
2018-12-10 16:35:57 +0800

    第二类目标

---
+++ 毕设代码/第二类目标/trajectory2.m
@@ -0,0 +1,19 @@
+clear;
+clc;
+R = 6371;
+v0 = 300;
+a = 200;
+theta = 30*pi/180;
+phy = 60*pi/180;
+lat0 = 20;
+lon0 = 10;
+v = zeros;
+x1 = [];
+y1 = [];
+z1 = [];
+for t = 0:0.005:1
+    
+
+end
+plot3(x1,y1,z1),xlabel('纬度'),ylabel('经度'),zlabel('高度');
+grid on;
