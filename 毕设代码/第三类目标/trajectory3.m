146d8cacaf3484510ea69bc870a2546e07f76f0d
610983691 <tongjie.job@foxmail.com>
2018-12-12 14:46:23 +0800

    完成多类别目标的运动模型分析； 在之前代码基础上进行完成代码编写整理；

---
+++ 毕设代码/第三类目标/trajectory3.m
@@ -0,0 +1,26 @@
+clear;
+clc;
+R = 6371;
+v0 = 700;
+a = 200;
+theta = 30*pi/180;
+phy = 60*pi/180;
+lat0 = 20;
+lon0 = 10;
+h0 = 700;
+v = zeros(1,201);
+lat = zeros(1,201);
+lon = zeros(1,201);
+h = zeros(1,201);
+v(1) = v0;
+lat(1) = lat0;
+lon(1) = lon0;
+h(1) = h0;
+for t = 0.005:0.005:1
+    v(round(t/0.005+1)) = v0 - a*t;
+    h(round(t/0.005+1)) = h(round(t/0.005))-(v(round(t/0.005))*0.005-1/2*a*0.005^2)*sin(phy);
+    lat(round(t/0.005+1)) = lat(round(t/0.005))+v(round(t/0.005))*0.005*cos(theta)/(R+h(round(t/0.005)));
+    lon(round(t/0.005+1)) = lon(round(t/0.005))+v(round(t/0.005))*0.005*sin(theta)/(R+h(round(t/0.005)))/cos(lat(round(t/0.005+1)));
+end
+plot3(lat,lon,h),xlabel('纬度'),ylabel('经度'),zlabel('高度');
+grid on;
