436fd749e839ad8d9f7d3994f326dc0a43654e21
610983691 <tongjie.job@foxmail.com>
2019-03-29 21:02:55 +0800

    完成飞机的分布，从地图上来看是对的

--- 毕设代码/代码/plane_dis2.m
+++ 毕设代码/代码/plane_dis2.m
@@ -7,29 +7,44 @@ rs = [(highs+6371)*sin(lats*pi/180)*cos(lons*pi/180),(highs+6371)*sin(lats*pi/18
 space = 6371*acos(6371/7071);%ÇòÃæ¾àÀë°ë¾¶
 r = space*sqrt(rand(1,N1));
 phy = 2*pi*rand(1,N1);
-plane_lon_path = zeros(1,N1);
-plane_lat_path = zeros(1,N1);
+num1 = 500;
+plane_lon_path = zeros(1,N1+num1);
+plane_lat_path = zeros(1,N1+num1);
 x = zeros(1,N1);
 y = zeros(1,N1);
 lon_change = zeros(1,N1);
 lat_change = zeros(1,N1);
 d = zeros(1,N1);
+lon = zeros(1,N1+num1);
+lat = zeros(1,N1+num1);
 for i = 1:N1
     x(i)= r(i).*cos(phy(i));
     y(i) =r(i).*sin(phy(i));
     lon_change(i) = x(i)/(2*pi*6371*cos(90-lats)/360);
     lat_change(i) = y(i)/(2*pi*6371/360);
-    lon = mod(lons+lon_change(i),360);
-    lat = mod(lats+lat_change(i),180);
-    r_plane = [6371*sin(lat*pi/180)*cos(lon*pi/180),6371*sin(lat*pi/180)*sin(lon*pi/180),6371*cos(lat*pi/180)];
+    lon(i) = mod(lons+lon_change(i),360);
+    lat(i) = mod(lats+lat_change(i),180);
+    r_plane = [6371*sin(lat(i)*pi/180)*cos(lon(i)*pi/180),6371*sin(lat(i)*pi/180)*sin(lon(i)*pi/180),6371*cos(lat(i)*pi/180)];
     d(i) = norm(r_plane-rs);
-    if lon>180
-       plane_lon_path(i) = lon-360;
+    if lon(i)>180
+       plane_lon_path(i) = lon(i)-360;
     else
-       plane_lon_path(i) = lon;
+       plane_lon_path(i) = lon(i);
     end
-    plane_lat_path(i) = 90-lat;
+    plane_lat_path(i) = 90-lat(i);
 end
+% goss_center_info = [lon(1,randi(N1)),lat(1,randi(N1))];
+% for i = 1:num1
+%     lon(1,N1+i) = mod(goss_center_info(1,1)+randn(),360);
+%     lat(1,N1+i) = mod(goss_center_info(1,2)+randn(),180);
+%     if lon(N1+i)>180
+%        plane_lon_path(N1+i) = lon(N1+i)-360;
+%     else
+%        plane_lon_path(N1+i) = lon(N1+i);
+%     end
+%     plane_lat_path(N1+i) = 90-lat(N1+i);
+% end
+
 t = 0:0.02:2*pi;
 plot(space*cos(t),space*sin(t),'r');
 axis square
