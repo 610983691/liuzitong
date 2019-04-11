897f8493be617b0f5768273a0af3a82d65b46add
610983691 <tongjie.job@foxmail.com>
2019-04-11 22:21:15 +0800

    论文

--- 毕设代码/代码/ppmencode.m
+++ 毕设代码/代码/ppmencode.m
@@ -16,8 +16,8 @@ cp = [ones(1, Fs/Rs/2), zeros(1, Fs/Rs/2), ones(1, Fs/Rs/2), zeros(1,2* Fs/Rs), 
 t = (0 : 120*Fs/Rs-1)/Fs;
 phase = rand(1)*360*pi/180;
 %频率抖动和多普勒频移
-car = cos(2*pi*(Fc_mid+doppler_f+((rand(1))*2-1)*10^5)*t+phase );
-ppm = cp.* car;
-% ppm = cp;
+% car = cos(2*pi*(Fc_mid+doppler_f+((rand(1))*2-1)*10^5)*t+phase );
+% ppm = cp.* car;
+ppm = cp;
 
 end
